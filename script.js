15cb96c3709b905b559dbeb2caacfdb9b1faa7e2
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-10-10 11:26:13 +0500

    problem 4

--- script.js
+++ script.js
@@ -117,10 +117,55 @@ Explanation: The optimal solution is to remove the last two elements to reduce x
  * @return {null|boolean|number|string|Array|Object}
  */
 // Array.prototype.last = function() {
-//     return this.length===0?-1:this.pop()
-// };
+    //     return this.length===0?-1:this.pop()
+    // };
+    
+    /**
+     * const arr = [1, 2, 3];
+ * arr.last(); // 3
+    */
+   
+   
+   
+   
+   // *************Problem 4*******************
 
 /**
- * const arr = [1, 2, 3];
- * arr.last(); // 3
+ * 
+ * 
+ * Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+ 
+
+Example 1:
+
+Input: 
+n = 10 
+["call","call","call"]
+Output: [10,11,12]
+Explanation: 
+counter() = 10 // The first time counter() is called, it returns n.
+counter() = 11 // Returns 1 more than the previous time.
+counter() = 12 // Returns 1 more than the previous time.
+ * 
+ */
+
+//solution
+
+/**
+//  * @param {number} n
+//  * @return {Function} counter
+//  */
+// var createCounter = function(n) {
+    
+//     return function() {
+//         return n++
+//     };
+// };
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
  */
