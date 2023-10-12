39bd7d1dd3bbfe05851d8a84dd957740b49f0b20
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-10-12 10:03:16 +0500

    problem 5

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ counter() = 12 // Returns 1 more than the previous time.
 // sleep(100).then(() => {
 //   console.log(Date.now() - t); // 100
 // });
-const myPromise = new Promise((resolve, reject) => {
+// const myPromise = new Promise((resolve, reject) => {
     // This is the executor function.
     // It takes two arguments: resolve and reject.
 
@@ -195,22 +195,22 @@ const myPromise = new Promise((resolve, reject) => {
     // If there's an error, call reject with an error message.
 
     // Example:
-    setTimeout(() => {
-        const randomNumber = Math.random();
-        if (randomNumber > 0.5) {
-            resolve(randomNumber); // Resolve with a value
-        } else {
-            reject("Random number is too small."); // Reject with an error message
-        }
-    }, 1000);
-});
+//     setTimeout(() => {
+//         const randomNumber = Math.random();
+//         if (randomNumber > 0.5) {
+//             resolve(randomNumber); // Resolve with a value
+//         } else {
+//             reject("Random number is too small."); // Reject with an error message
+//         }
+//     }, 1000);
+// });
 
 // Using the promise:
-myPromise.then(result => {
-    console.log("Resolved:", result);
-}).catch(error => {
-    console.error("Rejected:", error);
-});
+// myPromise.then(result => {
+//     console.log("Resolved:", result);
+// }).catch(error => {
+//     console.error("Rejected:", error);
+// });
 
 
 
@@ -223,22 +223,67 @@ myPromise.then(result => {
 
 
 
-const newPromise = new Promise((res, re) => {
-    setTimeout(() => {
-        const random = Math.random() * 2
-        if (random > 1) {
-            res(random)
-        }
-        else {
-            rej("this is not ok F")
-        }
-    }, 5000);
-})
-newPromise.then(result => {
-    console.log("resul", result);
-}).catch(error => {
-    console.log("error", error);
-})
+// const newPromise = new Promise((res, re) => {
+//     setTimeout(() => {
+//         const random = Math.random() * 2
+//         if (random > 1) {
+//             res(random)
+//         }
+//         else {
+//             rej("this is not ok F")
+//         }
+//     }, 5000);
+// })
+// newPromise.then(result => {
+//     console.log("resul", result);
+// }).catch(error => {
+//     console.log("error", error);
+// })
 
 
 
+
+    // *************Problem 6*******************
+
+//     Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+
+// A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+// If the length of the array is 0, it should return init.
+
+// Please solve it without using the built-in Array.reduce method.
+
+ 
+
+// Example 1:
+
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr; }
+// init = 0
+// Output: 10
+// Explanation:
+// initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10
+// The final answer is 10.
+
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+// var reduce = function (nums, fn, init) {
+//     var val = init
+
+//     for (const num of nums) {
+//         console.log("value of val before is", val, "num=", num)
+//         val = fn(val, num)
+//         console.log("value of val is", val)
+//     }
+//     return val
+// }
