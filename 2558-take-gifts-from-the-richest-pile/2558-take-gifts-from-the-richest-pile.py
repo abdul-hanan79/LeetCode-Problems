1d46917024c0f525769c33d2179fc4762ff70cdd
Abdul Hanan <abdulhanan3279@gmail.com>
2025-01-03 10:50:21 +0500

    Update 2558-take-gifts-from-the-richest-pile.py

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -7,14 +7,9 @@ class Solution:
         heap = []
         for gift in gifts:
             heapq.heappush(heap, -gift)
-        i = 0
-        while i < k:
+        while  k:
             pile = math.floor(math.sqrt(-heapq.heappop(heap)))
             heapq.heappush(heap, -pile)
-            i += 1
-        # Convert all values to positive
-        result = [abs(num) for num in heap]
+            k -= 1
 
-        # Sum the positive values
-        totalSum = sum(result)
-        return totalSum
+        return -sum(heap)
