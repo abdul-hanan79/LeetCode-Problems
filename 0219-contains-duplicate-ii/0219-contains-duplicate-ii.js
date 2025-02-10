86ed4718308a1d347757202a33ef1433d6d85304
Abdul Hanan <abdulhanan3279@gmail.com>
2025-02-10 10:46:51 +0500

    Update 0219-contains-duplicate-ii.js

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -13,3 +13,19 @@ var containsNearbyDuplicate = function (nums, k) {
     }
     return false
 };
+
+
+
+// ts
+
+// function containsNearbyDuplicate(nums: number[], k: number): boolean {
+//     const seen = {}
+//     for (let i = 0; i < nums.length; i++) {
+//         const val = nums[i]
+//         if (seen.hasOwnProperty(val) && i - seen[val] <= k) {
+//             return true
+//         }
+//         seen[val] = i
+//     }
+//     return false
+// };
