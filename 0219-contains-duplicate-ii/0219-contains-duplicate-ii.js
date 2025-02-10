fff897e381e77024c092028b71534e7ffc1e7f57
Abdul Hanan <abdulhanan3279@gmail.com>
2025-02-10 10:44:58 +0500

    Time: 1617 ms (5.03%), Space: 57.2 MB (98.67%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -5,9 +5,8 @@
  */
 var containsNearbyDuplicate = function (nums, k) {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
+        for (let j = i + 1; (j < nums.length && j <= i+k); j++) {
             if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
-
                 return true
             }
         }
