fc34e7a70c59ac78395c54d39f29767e265b5f75
Abdul Hanan <abdulhanan3279@gmail.com>
2025-02-25 10:35:42 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+    g.sort((a, b) => b - a)
+    s.sort((a, b) => b - a)
+    let contentChild = 0;
+    for (let i = 0; i < g.length; i++) {
+        for (let j = 0; j < s.length; j++) {
+            if (s[j] >= g[i]) {
+                s[j] -= 1
+                contentChild++
+                break
+            }
+        }
+    }
+    return contentChild
+};
