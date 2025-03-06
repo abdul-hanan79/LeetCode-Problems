5ef2ba905f5f1d94139f5e6dd2a689cfb9e490c1
Abdul Hanan <abdulhanan3279@gmail.com>
2025-03-06 10:11:56 +0500

    Time: 4 ms (85.71%), Space: 55.8 MB (50%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.js
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.js
@@ -4,12 +4,8 @@
  */
 var isValid = function (s) {
     while (s.length) {
-        console.log("s",s)
-        console.log("indexOf",s.indexOf("abc"))
-        
-
         if (s.indexOf("abc") == -1) break
-        s=s.replaceAll("abc","")
+        s = s.replaceAll("abc", "")
     }
     return !s.length
 };
