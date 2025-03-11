0dcccae11d5e56bd0bff4a17944d3f3bf4347a3d
Abdul Hanan <abdulhanan3279@gmail.com>
2025-03-11 09:52:39 +0500

    Time: 18 ms (15.75%), Space: 62.6 MB (27.79%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function (s) {
+    let stack = []
+    for (let i of s) {
+        if (stack[stack.length - 1] == i) {
+            stack.pop()
+        } else {
+            stack.push(i)
+        }
+    }
+    return stack.join("")
+};
