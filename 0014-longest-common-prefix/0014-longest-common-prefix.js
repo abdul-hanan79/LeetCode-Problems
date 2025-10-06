979f916fbae83bc1513e661ce3b75a371104f1b1
Abdul Hanan <abdulhanan3279@gmail.com>
2025-10-06 11:48:15 +0500

    Time: 2 ms (42.87%), Space: 56.2 MB (20.42%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,17 +3,16 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    if (!strs.length) {
-        return ""
-    }
-    let base = strs[0]
-    const rest = strs.slice(1); // <-- compute once
+    if (!strs.length) return "";
+
+    let base = strs[0];
     for (let i = 0; i < base.length; i++) {
-        for (let word of rest) {
-            if (i === word.length || word[i] != base[i]) {
-                return base.slice(0, i)
+        for (let j = 1; j < strs.length; j++) { 
+            let word = strs[j];
+            if (i === word.length || word[i] !== base[i]) {
+                return base.slice(0, i);
             }
         }
     }
-    return base
+    return base;
 };
