345239ec14d4e5bc92042b8bf7355c0332da6a9d
Abdul Hanan <abdulhanan3279@gmail.com>
2025-11-04 12:47:18 +0500

    Time: 0 ms (100%), Space: 54.3 MB (80.63%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -0,0 +1,14 @@
+function climbStairs(n: number): number {
+    let memo = {}
+    const countSteps = (n, memo = {}) => {
+        if (n <= 1) {
+            return 1
+        };
+        if (memo[n]) {
+            return memo[n]
+        }
+        memo[n] = countSteps(n - 1, memo) + countSteps(n - 2, memo)
+        return memo[n]
+    }
+    return countSteps(n, memo)
+};
