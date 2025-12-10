dd3e35b9f96984cffaa209ff26d9673b2f77a506
Abdul Hanan <abdulhanan3279@gmail.com>
2025-12-10 11:58:52 +0500

    Time: 1 ms (70.57%), Space: 59.4 MB (32.59%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -11,26 +11,24 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-    if (!root) {
-        return []
-    }
-    let result = []
-    const queue = [root]
-    while (queue.length > 0) {
-        const currentLevel = [];
-        const levelSize = queue.length;
-        for (let i = 0; i < levelSize; i++) {
-            const node = queue.shift(); // Dequeue the front element
-            currentLevel.push(node.val);
+    if (!root) return [];
 
-            if (node.left) {
-                queue.push(node.left); // Enqueue left child
-            }
-            if (node.right) {
-                queue.push(node.right); // Enqueue right child
-            }
+    let queue = [root];
+    let result = [];
+
+    while (queue.length > 0) {
+        let levelSize = queue.length
+        let level = []
+
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift()
+            level.push(node.val)
+
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
         }
-        result.push(currentLevel);
+        result.push(level)
     }
-    return result
+
+    return result;
 };
