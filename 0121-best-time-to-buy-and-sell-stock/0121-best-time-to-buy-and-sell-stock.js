5bb4d0c33b2323d4c0083df0330fb2ef57d9c913
Abdul Hanan <abdulhanan3279@gmail.com>
2026-07-21 15:49:03 +0500

    Time: 4 ms (32.47%), Space: 65.3 MB (51.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,17 +3,29 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let profit = 0
-    let cheapestPrice = prices[0]
-    for (let index in prices) {
-        if (index > 0) {
-            if (prices[index] < cheapestPrice) {
-                cheapestPrice = prices[index]
-            }
-            if (prices[index] - cheapestPrice > profit) {
-                profit = prices[index] - cheapestPrice
-            }
-        }
+
+    let minimumPrice = prices[0]
+    let maxProfit = 0
+
+    for (let i = 1; i < prices.length; i++) {
+        const price = prices[i]
+        minimumPrice = Math.min(minimumPrice, price)
+        const profit = price - minimumPrice
+        maxProfit = Math.max(maxProfit, profit)
     }
-    return profit
+    return maxProfit
+
 };
+
+
+// I need to maximize the profit, to achieve this I have the first buy the on the smallest day and less on the largest day that comes after the smallest. 
+
+// Questions: How I select the smallest day? 
+// Profit= next largest day - smnallest day
+// [7,1,5,3,6,4]
+// 7 is not smallest
+// 1 is smallest it selected as a buy stock day
+// 5 not largest
+// 3 not largest
+// 6 it is the largest stock day, so 6-1 =5 (this one is the profit)
+//  if profit is in minu then return 0
