2adac23e0d7fe87c10af4a27c59a385bab988ec6
Abdul Hanan <abdulhanan3279@gmail.com>
2026-07-22 14:55:19 +0500

    Time: 15 ms (32.15%), Space: 63.3 MB (37.85%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -4,12 +4,19 @@
  */
 var removeDuplicates = function (s) {
     let stack = []
-    for (let i of s) {
-        if (stack[stack.length - 1] == i) {
+    for (let ch of s) {
+        if (stack.at(-1) == ch) {
             stack.pop()
-        } else {
-            stack.push(i)
+        }
+        else {
+            stack.push(ch)
         }
     }
     return stack.join("")
 };
+
+
+// Break down problem:
+// 1. I'll start loop on s, 
+// push first element in the stack, if stack length greater than 0, then see that what was the last element, if it same as current, then remove that, othwerise just push. the elemnt, 
+// 2. at the end just return the final stadk
