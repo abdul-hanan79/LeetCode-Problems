69182a15cb62df560647c66e0dd3471707af92df
Abdul Hanan <abdulhanan3279@gmail.com>
2026-07-23 15:10:11 +0500

    Time: 42 ms (27.04%), Space: 84.4 MB (49.47%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,28 +4,28 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function (nums, k) {
+    let map = new Map()
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; (j < nums.length && j <= i+k); j++) {
-            if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+        if (map.has(nums[i])) {
+            if (Math.abs(map.get(nums[i]) - i) <= k) {
                 return true
             }
         }
+        
+        map.set(nums[i], i)
     }
     return false
 };
 
 
+// input: nums=[1,2,3,1], k=3
+// output: true/false
+// problem statement: prlbem asked to see the if the array containts duplicate in such a way that , nums[i]==nums[j] and abs(i-j)<=k, its mean I need to focus on K.
+//  constraints: k<=0, nums[i] can be negative and positive
+// pattern map, 
+// break down problem:
+// 1. initialize the map
+// 2. save the number agains map, if containts then check that index of current and saved number is abs(i-j)<=k,
 
-// ts
-
-// function containsNearbyDuplicate(nums: number[], k: number): boolean {
-//     const seen = {}
-//     for (let i = 0; i < nums.length; i++) {
-//         const val = nums[i]
-//         if (seen.hasOwnProperty(val) && i - seen[val] <= k) {
-//             return true
-//         }
-//         seen[val] = i
-//     }
-//     return false
-// };
+// edge cases:
