9f6cadeed80c23a2a610369d6d003bbb4a058cf4
Abdul Hanan <abdulhanan3279@gmail.com>
2026-07-23 10:52:21 +0500

    Time: 2 ms (69.52%), Space: 57.1 MB (70.12%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,27 +3,29 @@
  * @return {number}
  */
 var pivotIndex = function (nums) {
-    let leftSum = 0;
-    let rightSum = 0;
-    for (let j = 0; j < nums.length; j++) {
-        if (j > 0) {
-            rightSum += nums[j]
-        }
+    let prefixSum = []
+    prefixSum[0] = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+        prefixSum[i] = prefixSum[i - 1] + nums[i]
     }
-
-    console.log("right sum", rightSum)
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0) {
-            rightSum = rightSum - nums[i]
-
-            leftSum += nums[i - 1]
-        }
-        if (rightSum == leftSum) {
-            return i
+    let right = prefixSum.length - 1
+    for (let j = 0; j < prefixSum.length; j++) {
+        if ((j > 0 && prefixSum[right] - prefixSum[j] == prefixSum[j - 1] || (j == 0 && prefixSum[right] - prefixSum[j] == 0))) {
+            return j
         }
 
     }
     return -1
-    // console.log("value", leftSum)
-    // console.log("value right", rightSum)
 };
+
+// Input: nums=[1,7,3,6,5,6]
+// Output: result=3 // return the pivot index 
+// Problem: I need to find the left most pivot index, where sum of left side of array is equal to the sum of right side of array
+// pattern prefix sum. because I need to know the range 
+
+//  Breaking the problem into steps:
+// 1. create the prefix sum of input
+// [1,7,3,6,5,6]
+// [1,8,11,17,22,28]
+// then current index - total sum, if it is equal to the index-1 value, then this is pivot index
